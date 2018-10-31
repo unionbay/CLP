ac9995e18625f0a16f3c98d3b4e9af699bc2b4a7
anh-nguyen <hoanganh4388@gmail.com>
2018-10-31 16:19:00 +0700

    Add RotationComparator

---
+++ CLP-A01/src/main/java/anhnh34/com/vn/model/RotationComparator.java
@@ -0,0 +1,116 @@
+package anhnh34.com.vn.model;
+
+import java.util.Comparator;
+
+public class RotationComparator implements Comparator<Rotation> {
+	private static String orderStr;
+
+	public RotationComparator() {
+		if (orderStr == null || orderStr.isEmpty()) {
+			orderStr = Utility.getInstance().getConfigValue("selected_rotation");
+		}
+	}
+
+	@Override
+	public int compare(Rotation r1, Rotation r2) {
+		switch (orderStr) {
+		case Constant.ROTATION_SORT_ZYX:
+			return firstSortRule(r1, r2);
+		case Constant.ROTATION_SORT_ZXY:
+			return secondSortRule(r1, r2);
+		case Constant.ROTATION_SORT_YXZ:
+			return thirdSortRule(r1, r2);
+		default:
+			return 0;
+
+		}
+	}
+
+	private int thirdSortRule(Rotation r1, Rotation r2) {		
+		
+		if(r1.getWidth() > r2.getWidth()) {
+			return -1;
+		}
+		
+		if(r1.getWidth() < r2.getWidth()) {
+			return 1;
+		}
+		
+		
+		if(r1.getLength() > r2.getLength()) {
+			return -1;
+		}
+		
+		if(r1.getLength() < r2.getLength()) {
+			return 1;
+		}		
+				
+		if(r1.getHeight() > r2.getHeight()) {
+			return -1;
+		}
+		
+		if(r1.getHeight() < r2.getHeight()) {
+			return 1;
+		}
+				
+		return 0;
+	}
+
+	private int secondSortRule(Rotation r1, Rotation r2) {
+		
+		if(r1.getHeight() > r2.getHeight()) {
+			return -1;
+		}
+		
+		if(r1.getHeight() < r2.getHeight()) {
+			return 1;
+		}
+		
+		if(r1.getLength() > r2.getLength()) {
+			return -1;
+		}
+		
+		if(r1.getLength() < r2.getLength()) {
+			return 1;
+		}		
+		
+		if(r1.getWidth() > r2.getWidth()) {
+			return -1;
+		}
+		
+		if(r1.getWidth() < r2.getWidth()) {
+			return 1;
+		}
+		
+		return 0;
+	}
+
+	private int firstSortRule(Rotation r1, Rotation r2) {
+		if(r1.getHeight() > r2.getHeight()) {
+			return -1;
+		}
+		
+		if(r1.getHeight() < r2.getHeight()) {
+			return 1;
+		}
+		
+		if(r1.getWidth() > r2.getWidth()) {
+			return -1;
+		}
+		
+		if(r1.getWidth() < r2.getWidth()) {
+			return 1;
+		}
+		
+		if(r1.getLength() > r2.getLength()) {
+			return -1;
+		}
+		
+		if(r1.getLength() < r2.getLength()) {
+			return 1;
+		}		
+		
+		return 0;
+	}
+
+}
